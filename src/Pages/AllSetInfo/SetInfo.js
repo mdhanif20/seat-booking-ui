275b6e3768f5f647a7b35d0eb528092a1fd87bb0
mdhainf20 <mdhanif115825@gmail.com>
2022-12-29 18:24:45 +0600

    add seat booking ui

---
+++ src/Pages/AllSetInfo/SetInfo.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import BookingModal from '../BookingModal/BookingModal';
+import Button from '@mui/material/Button';
+
+const SetInfo = () => {
+    const [allSeat,setAllSeat] = useState([]);
+    const [modalData,setModalData] = useState({});
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = (e,data) => {
+        setOpen(e);
+        setModalData(data);
+    }
+    const handleClose = () => setOpen(false);
+    useEffect(()=>{
+        fetch("http://localhost:5000/allSeatInfo")
+        .then(res => res.json())
+        .then(data => setAllSeat(data))
+    },[handleOpen])
+    return (
+        <Box sx={{mx:2,my:5}}>
+            <Box>
+                <h2>Book The Seat</h2>
+            </Box>
+            <Box
+            style={{
+                display:"grid",
+                gridTemplateColumns:"1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr"
+            }}
+            >
+                {
+                    allSeat.map((seat)=><Paper
+                    key={seat._id}
+                    sx={{height: 80,width: 120,    
+                    backgroundColor:seat.booked?"#b4a258":"#ffcc00",
+                    p:1,borderRadius:"10px",m:1,cursor:"pointer"}}> 
+                     <Typography sx={{fontWeight:600}} variant="h6" gutterBottom>
+                        {seat.seatNo}
+                    </Typography>
+                    {
+                        seat.booked?<Typography sx={{fontWeight:500}} variant="body1" gutterBottom>
+                        Already Booked
+                        </Typography>:
+                        <Button style={{backgroundColor:'#23272b',color:"#fff"}} onClick={()=>handleOpen(true,seat)}>Book Now</Button>
+                    }
+                    
+                  </Paper>)
+                }
+                
+            </Box>
+            <BookingModal
+            open={open}
+            handleClose={handleClose}
+            modalData={modalData}
+            >
+
+            </BookingModal>
+        </Box>
+    );
+};
+
+export default SetInfo;
---
+++ src/Pages/BookingModal/BookingModal.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+    borderRadius:"10px"
+  };
+
+const BookingModal = ({open,handleClose,modalData}) => {
+    const defaultInfo = {
+        name:"",
+        email:"",
+        phoneNumber:""
+    }
+    const [bookingInfo,setBookingInfo] = useState(defaultInfo); 
+    const handleOnBlur = e =>{
+        const field = e.target.name;
+        const value = e.target.value;
+        const newInfo = {...bookingInfo};
+        newInfo[field] = value
+        setBookingInfo(newInfo);
+    }
+    const bookinData = {
+        ...bookingInfo,
+        seatNo:`${modalData.seatNo}`,
+        booked: true
+    }
+
+    const bookingSeat = (id) =>{
+        if(bookinData.name == "" && bookinData.email == "" && bookinData.phoneNumber==""){
+            alert("Please, fill up properly name, email and phone number")
+        }else{
+            const url = `http://localhost:5000/bookingSeat/${id}`;
+            fetch(url,{
+                method:"PUT",
+                headers:{
+                    'content-type':"application/json"
+                },
+                body: JSON.stringify(bookinData)
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.acknowledged){
+                    alert("Booked Successfully!!")
+                }
+            })
+        } 
+    }
+
+    return (
+        <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+        style={{border:0}}
+      >
+            <Box sx={style}>
+            <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
+                Seat No: {modalData.seatNo}
+            </Typography>
+            <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
+                <TextField
+                sx={{width:"100%",my:1}}
+                label="Your Name"
+                name="name"
+                onBlur={handleOnBlur}
+                id="outlined-size-small"
+                defaultValue="Name"
+                size="small"
+                />
+            </Typography>
+            <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
+                <TextField
+                sx={{width:"100%",my:1}}
+                label="Your Email"
+                name="email"
+                onBlur={handleOnBlur}
+                id="outlined-size-small"
+                defaultValue="Email"
+                size="small"
+                />
+            </Typography>
+            <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
+                <TextField
+                sx={{width:"100%",my:1}}
+                label="Enter Your Phone Number"
+                name="phoneNumber"
+                onBlur={handleOnBlur}
+                id="outlined-size-small"
+                defaultValue="Phone Number"
+                size="small"
+                />
+            </Typography>
+            <Button onClick={()=>bookingSeat(`${modalData._id}`)} style={{backgroundColor:'#23272b',color:"#fff",mt:2}}>Book Now</Button>
+            </Box>
+      </Modal>
+    );
+};
+
+export default BookingModal;
